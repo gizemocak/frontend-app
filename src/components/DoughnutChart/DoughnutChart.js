01bc8fff5101b96702afe38e69c7d04267ee9e58
gizemocak <gizem_ocak@outlook.com>
2019-08-16 14:02:35 -0400

    initial commit

--- src/components/DoughnutChart/DoughnutChart.js
+++ src/components/DoughnutChart/DoughnutChart.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react';
-import { Doughnut } from 'react-chartjs-2';
-
-import './DoughnutChart.scss';
+import React, { Component } from "react";
+import { Doughnut } from "react-chartjs-2";
+import "./DoughnutChart.scss";
 
 class DoughnutChart extends Component {
-    render(){
-        let data = this.getData();
-        return (
-            <div className="doughnut-container">
-                <div className="doughnut-wrapper">
-                    <Doughnut
-                        data={data}
-                        width={300}
-                        height={300}
-                        options={this.getOptions()}
-                        legend={this.getLegend()}
-                    />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    let data = this.getData();
+    return (
+      <div className="doughnut-container">
+        <div className="doughnut-wrapper">
+          <Doughnut
+            data={data}
+            width={300}
+            height={300}
+            options={this.getOptions()}
+            legend={this.getLegend()}
+          />
+        </div>
+      </div>
+    );
+  }
 
-
-    getData(){
-        return {
-            datasets: [{
-                data: [10, 20, 30],
-                backgroundColor: Object.values(this.getLabelsAndColors())
-            }],
-            labels: Object.keys(this.getLabelsAndColors())
-        };
-    }
-
-    getOptions(){
-        return {
-            responsive: true,
-            maintainAspectRatio: true
+  getData() {
+    return {
+      datasets: [
+        {
+          data: [10, 20, 30],
+          backgroundColor: Object.values(this.getLabelsAndColors())
         }
-    }
+      ],
+      labels: Object.keys(this.getLabelsAndColors())
+    };
+  }
 
-    getLegend(){
-        return {
-            position: 'bottom'
-        };
-    }
+  getOptions() {
+    return {
+      responsive: true,
+      maintainAspectRatio: true
+    };
+  }
 
-    getLabelsAndColors() {
-        return {
-            'Red': '#FF6384',
-            'Blue': '#36A2EB',
-            'Yellow': '#FFCE56'
-        }
-    }
+  getLegend() {
+    return {
+      position: "bottom"
+    };
+  }
+
+  getLabelsAndColors() {
+    return {
+      Red: "#FF6384",
+      Blue: "#36A2EB",
+      Yellow: "#FFCE56"
+    };
+  }
 }
 
 export default DoughnutChart;
